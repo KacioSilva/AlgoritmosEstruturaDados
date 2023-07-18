de44aee2d3dbda3615564ba3f04b0b33dea85567
KácioSilva <101821745+KacioSilva@users.noreply.github.com>
2023-07-18 02:03:00 -0300

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        tabelaHash hashTable = new tabelaHash();
+        hashTable.adicionar("Elemento 1", "A");
+        hashTable.adicionar("Elemento 2", "B");
+        hashTable.adicionar("Elemento 3", "C");
+        hashTable.adicionar("Elemento 4", "D");
 
-        System.out.println("Hello world!");
-        System.out.println("testando");
+        System.out.println(hashTable.get("Elemento 4"));
+        System.out.println(hashTable.get("Elemento 3"));
+        System.out.println(hashTable.get("Elemento 2"));
+        System.out.println(hashTable.get("Elemento 1"));
 
     }
-
 }
--- src/tabelaHash.java
+++ src/tabelaHash.java
@@ -1,65 +1,74 @@
 public class tabelaHash {
-    int size;
-    int tableSize = 10;
-    HashTableEntry[] vetor;
+    private tabelaHashEntry[] vetor;
+    private int sizeTable = 10;
+    private int size;
 
     public tabelaHash(){
+        this.vetor = new tabelaHashEntry[sizeTable];
         this.size = 0;
-        this.vetor = new HashTableEntry[tableSize];
     }
-    public void adicionar(String key, String valor){
-        if(this.size / this.tableSize > 0.5){
+
+    public void adicionar(String key, String value){
+        if(this.size / this.sizeTable > 0.5){
             reHash();
+        }else{
+            int index = defineIndex(key);
+            this.vetor[index] = new tabelaHashEntry(key, value);
+            size++;
         }
-        int index = defineIndex(key);
-        vetor[index] = new HashTableEntry(key, valor);
-        size++;
+    }
+    private void reHash(){
+        tabelaHashEntry[] newVetor = this.vetor;
+        int newTableSize = this.sizeTable * 2;
+
+        this.vetor = new tabelaHashEntry[newTableSize];
+        this.sizeTable = newTableSize;
+
+        for(tabelaHashEntry entry : newVetor){
+            adicionar(entry.getKey(), entry.getValue());
+        }
     }
 
-    public void remove(String key){
-
-    }
-    public String get(String key){
-        return null;
-    }
     public int getSize(){
         return this.size;
     }
-    private void reHash(){
-
+    public String get(String key){
+        int index = defineIndex(key);
+        return this.vetor[index] == null ? null : vetor[index].getValue();
     }
+
     private int defineIndex(String key){
-        int index = key.hashCode() % this.vetor.length;
-        while(this.vetor[index] != null && this.vetor[index].getKey() != key){
-            if(index == vetor.length-1){
+        int hasCode = key.hashCode() < 0 ? key.hashCode() * (-1) : key.hashCode();
+        int index = hasCode % vetor.length;
+        while(vetor[index] != null && vetor[index].getKey() != key){
+            if(index == vetor.length - 1){
                 index = 0;
             }else{
                 index++;
             }
-        } return index;
+        }return index;
     }
-
-    public class HashTableEntry{
+    
+    public class tabelaHashEntry{
         private String key;
-        private String valor;
+        private String value;
 
-        public HashTableEntry(String key, String valor){
+        public tabelaHashEntry(String key, String value){
             super();
             this.key = key;
-            this.valor = valor;
+            this.value = value;
         }
-
         public String getKey(){
-            return key;
+            return this.key;
         }
         public void setKey(String key){
             this.key = key;
         }
-
-        public String getValor(){
-            return this.valor;
+        public String getValue(){
+            return this.value;
         }
-        public void setValor(String valor){
+        public void setValue(String value){
+            this.value = value;
         }
     }
 }
